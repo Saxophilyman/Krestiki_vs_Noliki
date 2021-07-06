b6a71e614e31e4141b1e9967ebd4a4b8f5a2b073
Saxophilyman <zd_@mail.ru>
2021-07-06 21:46:00 +0400

    Part_I "Hello message"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Приветствую вас на экспериментальной площадке \"Будущий супер программист\"!." +
+                "\nХотите ли сыграть в игру \"Крестики-нолики\"?"+
+                "\nВедите в консоль \"да\" или \"нет\"");
+        String s = null;
+        try{
+        s = reader.readLine();}
+        catch (Exception e){
+            System.out.println("Что-то пошло не так, извините");
+        }
+        if (!s.equals("да")&&!s.equals("нет")) {
+            System.out.println("Упс, кто-то ввёл что-то не так");
+        }
+
+        if (s.equals("да")){
+            System.out.println("Отлично! Ходит \"Первый игрок\"");}
+        if (s.equals("нет")){
+            System.out.println("Ну нет так нет");}
+        //break;
+
+
     }
 }
