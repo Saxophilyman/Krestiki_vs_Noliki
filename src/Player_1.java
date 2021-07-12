771908b08eb2e4afeef34955bbb57175b8ac5a5e
Saxophilyman <zd_@mail.ru>
2021-07-13 02:09:14 +0400

    Part_III "Details 1.1" (thoughts about two player)

---
+++ src/Player_1.java
@@ -0,0 +1,20 @@
+public class Player_1 {
+    String name = "Player_1";
+    String mark = "X";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+}
